a096297b417a6e369e605d23a09cbd91d195ae2c
Daniel Urdaneta <daniel.urdaneta02@gmail.com>
2021-06-08 09:49:43 -0500

    Analysis updated

--- Querys/queries.sql
+++ Querys/queries.sql
@@ -52,20 +52,83 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 PRIMARY KEY (emp_no)
 );
 
+-- Change titles primary key
 ALTER TABLE public.titles
     ADD CONSTRAINT pk_titles PRIMARY KEY (emp_no, title, to_date);
 SELECT * FROM employees;
 
+-- Make queries
 SELECT COUNT(first_name)
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 
-SELECT first_name, last_name
+SELECT first_name, last_name, emp_no
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
-SELECT * FROM retirement_info
+-- Create current employees table
+SELECT r.first_name, r.last_name, r.emp_no, d.to_date
+INTO current_emp
+FROM retirement_info as r
+LEFT JOIN dept_emp as d 
+ON r.emp_no = d.emp_no
+WHERE d.to_date = ('9999-01-01');
+
+
+-- Joining departments and dept_manager tables
+SELECT de.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as de
+INNER JOIN dept_manager as dm
+ON de.dept_no = dm.dept_no;
+
+SELECT count(ce.emp_no), de.dept_no
+INTO ret_emp_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no
+
+-- Create employee info table
+SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary, de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON e.emp_no = s.emp_no
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+-- Create manager info table
+SELECT ma.dept_no, de.dept_name, ma.emp_no, ce.first_name, ce.last_name, ma.from_date, ma.to_date
+INTO manager_info
+FROM current_emp AS ce
+INNER JOIN dept_manager AS ma
+ON ce.emp_no = ma.emp_no
+INNER JOIN departments AS de
+ON ma.dept_no = de.dept_no
+
+-- Create current retiring employees per department table
+SELECT de.emp_no, ce.first_name, ce.last_name, d.dept_name
+INTO ce_departments
+FROM dept_emp as de
+INNER JOIN current_emp as ce 
+ON de.emp_no = ce.emp_no
+INNER JOIN departments as d
+ON de.dept_no = d.dept_no
+ORDER BY dept_name
+
+-- Create current retiring employees for sales and development department
+SELECT *
+INTO sales_devel_info
+FROM ce_departments
+WHERE dept_name IN ('Sales', 'Development'); 
